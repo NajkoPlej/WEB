8370318f4efcf8b5e853789db7c03697a9527e87
Nicolas Gambaľ <nikolaskogambal@gmail.com>
2026-01-20 09:00:09 +0100

    pridal som hodnoty a vysledok bmi

--- BMI-J.S/script.js
+++ BMI-J.S/script.js
@@ -4,9 +4,17 @@ const vyska = prompt("Zadaj výška v metroch");
 console.log(vaha);
 console.log(vyska);
 
-const vysledok = vaha / vyska*vyska ;
+const bmi = vaha / vyska*vyska ;
 
-console.log(vysledok);
+console.log(bmi);
 
+if (bmi < 18.5) {
+    console.log("Podváha");
+} else if (bmi >= 18.5 && bmi < 25) {
+    console.log("Normálna váha");
+}  else if (bmi >= 25 && bmi < 30) {
+    console.log("Nadváha");
+} else (bmi >= 30 && bmi < 40)
+    console.log("Obezita");
 
 
