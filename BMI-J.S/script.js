d7617cd7aee76b66517c47ca9fe7eacbbf62287f
Nicolas Gambaľ <nikolaskogambal@gmail.com>
2026-01-20 08:28:17 +0100

    spravil som zaklad js na vypocet bmi

---
+++ BMI-J.S/script.js
@@ -0,0 +1,9 @@
+const vaha = prompt("Zadaj hmotnostť v kilogramoch");
+const vyska = prompt("Zadaj výška v metroch");
+
+console.log(vaha);
+console.log(vyska);
+
+const vysledok = vaha / vyska**2 ;
+
+console.log(vysledok)
