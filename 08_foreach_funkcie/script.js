454d130aec5e9d150b25d74553c23f1ce77f18b7
Nicolas Gambaľ <nikolaskogambal@gmail.com>
2026-04-28 09:30:39 +0200

    dokoncil som ulohu

--- 08_foreach_funkcie/script.js
+++ 08_foreach_funkcie/script.js
@@ -1,6 +1,26 @@
 const teploty = [12,5-6,1,-9,4,8];
 let spoluTeploty = 0;
+let priemer = 0;
+
+console.log(teploty);
+//console.log(`spoluTeploty = spoluTeploty + teplota;`)
 
 teploty.forEach(function(teplota){
-    spoluTeploty += spoluTeploty + teplota;
-}) //foreach == vytiahne z pola kazdy prvok, da sa do novej premennej "teplota"
+    spoluTeploty = spoluTeploty + teplota;
+    //console.log(`${spoluTeploty} = ${spoluTeploty} + ${teplota} ;`) //kontrolny výpis 
+    
+
+    if(teplota <= 0){
+        console.log(`${teplota}°C : Nebezpečenstvo námrazy`);
+    }
+    else if (0 < teplota && teplota <= 4){
+        console.log(`${teplota}°C : Kasovita cesta`);
+    }
+    else {
+        console.log(`${teplota}°C : Bez námrazy`);
+    }
+
+
+}); //foreach == cyklus, s ktorým prechádzame pole, vytiahne z pola kazdy prvok, da sa do novej premennej "teplota"
+priemer = spoluTeploty / teploty.length;
+//console.log(`Priemerna teplota je ${priemer.toFixed(2)}`); //zaokruhli priemer na 2 dessatinné miesta
