d89916b1b34e283089c6f91dee1127f0c52efa46
Nicolas Gambaľ <nikolaskogambal@gmail.com>
2026-04-28 08:54:44 +0200

    pridal som novu pracu for each kde som pridal js

---
+++ 08_foreach_funkcie/script.js
@@ -0,0 +1,6 @@
+const teploty = [12,5-6,1,-9,4,8];
+let spoluTeploty = 0;
+
+teploty.forEach(function(teplota){
+    spoluTeploty += spoluTeploty + teplota;
+}) //foreach == vytiahne z pola kazdy prvok, da sa do novej premennej "teplota"
