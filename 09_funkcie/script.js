7b714b26cb4dc1784bca458c8d628757e143f73f
Nicolas Gambaľ <nikolaskogambal@gmail.com>
2026-05-19 09:27:48 +0200

    hotovinko

--- 09_funkcie/script.js
+++ 09_funkcie/script.js
@@ -15,9 +15,6 @@ function pozdrav (){
     vysledok.style.color = 'darkgreen'; //pridam tomu vizual
     vysledok.style.fontweight = 'bold';
 }
-
-
-
 function zmenaFarbuPozadia (){
     const farby = ['#ff6b6b', '#8BDFDD', '#F48F68', '#FFE394', '#C9CAAC', '#CCD67F'];//pole kde je 5/6 farbieb a random vygeneruje jednu z nich
     let vysledok = document.getElementById('vysledok1');
@@ -29,38 +26,42 @@ function zmenaFarbuPozadia (){
 
 }
 
+
+
+
 function zvysPocitadlo(){
     pocet++; //zvys o jedna
     document.getElementById('pocitadlo').textContent = pocet; //napln ho aktualnou hodnotou
 
 }
-
 function znizPocitadlo(){
     pocet--; //zniz o jedna
     document.getElementById('pocitadlo').textContent = pocet;//napln ho aktualnou hodnotou
 
 }
-
 function resetujPocitadlo(){
     pocet = 0;
     document.getElementById('pocitadlo').textContent = pocet;
 }
 
 
+
+
 function zvacsFont(){
    const vysledokText = document.getElementById("vysledokText");
    vysledokText.style.fontSize = '30px';
 }
-
 function zmensFont(){
     const vysledokText = document.getElementById("vysledokText");
     vysledokText.style.fontSize = '12px';
 }
-
 function normalnyFont(){
     const vysledokText = document.getElementById("vysledokText");
     vysledokText.style.fontSize = '1.1em';
 }
+
+
+
 
 function nahodneCislo(){
     let vysledokNahodne = document.getElementById('vysledokNahodne');
@@ -68,7 +69,6 @@ function nahodneCislo(){
 
     vysledokNahodne.textContent = `Cislo ktoré som vybral je: ${nahodneCislo}`; 
 }
-
 function zmenaFarbyTextu(){
     let vysledokNahodne = document.getElementById('vysledokNahodne');
     const textFarba = ['#37353E', '#44444E', '#715A5A', '#D3DAD9', '#511D43', '#18230F'];//pole kde je 5/6 farbieb a random vygeneruje jednu z nich
@@ -85,7 +85,7 @@ function zmenaFarbyTextu(){
 
 
 
-const nahodnaFarbaBtn = document.getElementById('nahodnaFarbaBtn');
+
 const pozdravBtn = document.getElementById('pozdravBtn');
 const zmenaPozadiaBtn = document.getElementById('zmenFarbuBtn');
 
@@ -98,20 +98,24 @@ const zmensBtn = document.getElementById('zmensBtn');
 const normalnyBtn = document.getElementById('normalnyBtn');
 
 const nahodnecisloBtn = document.getElementById('nahodnecisloBtn');
+const nahodnaFarbaBtn = document.getElementById('nahodnaFarbaBtn');
 
 
+
+
+
+
+
+pozdravBtn.addEventListener('click', pozdrav); //dam tomu buttnu funkciu
+zmenFarbuBtn.addEventListener('click', zmenaFarbuPozadia);
 
 zvysBtn.addEventListener('click', zvysPocitadlo);
 znizBtn.addEventListener('click', znizPocitadlo);
 resetBtn.addEventListener('click', resetujPocitadlo);
-
-pozdravBtn.addEventListener('click', pozdrav); //dam tomu buttnu funkciu
-zmenFarbuBtn.addEventListener('click', zmenaFarbuPozadia);
 
 zvacsBtn.addEventListener('click', zvacsFont);
 zmensBtn.addEventListener('click', zmensFont);
 normalnyBtn.addEventListener('click', normalnyFont);
 
 nahodneCisloBtn.addEventListener('click', nahodneCislo);
-
 nahodnaFarbaBtn.addEventListener('click', zmenaFarbyTextu);
